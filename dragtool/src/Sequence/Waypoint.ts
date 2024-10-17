b70d661fd5c5e24873dae850ac249231c41f9a8a
desain <nicholassdesai@gmail.com>
2024-10-17 14:10:16 -0500

    Match waypoint border to ruler

--- dragtool/src/Sequence/Waypoint.ts
+++ dragtool/src/Sequence/Waypoint.ts
@@ -2,6 +2,8 @@ import { Item, Layer, Shape, buildShape } from "@owlbear-rodeo/sdk";
 import { MARKER_STROKE_WIDTH_DPI_SCALING, ZIndex } from "../constants";
 import { SequenceItem, buildSequenceItem } from "./SequenceItem";
 
+const WAYPOINT_STROKE = '#6F738F'; // same color as OBR thin rulers
+
 export type Waypoint = Shape & SequenceItem;
 
 export function createWaypoint(target: Item, layer: Layer, dpi: number, color: string): Waypoint {
@@ -12,6 +14,6 @@ export function createWaypoint(target: Item, layer: Layer, dpi: number, color: s
         .width(dpi / 4)
         .height(dpi / 4)
         .fillColor(color)
-        .strokeColor('gray')
+        .strokeColor(WAYPOINT_STROKE)
         .strokeWidth(dpi * MARKER_STROKE_WIDTH_DPI_SCALING));
 }
